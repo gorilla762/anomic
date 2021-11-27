5e09a17428a44aba66b51b2aec8e76cff2632a89
gorilla762 <95144273+gorilla762@users.noreply.github.com>
2021-11-27 19:41:45 +0000

    Add loading thingy

---
+++ load.lua
@@ -0,0 +1,10 @@
+--// load modules into the game when executed
+
+if game.PlaceId ~= 4581966615 then
+	warn('Halted execution, game is not compatible for the script.')
+elseif rconsole then
+	
+elseif not rconsole then
+	print('Loading modules.')
+	
+end
